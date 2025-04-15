24a304b5a5cdcd538a16faf38c5dd6c8c9dc617b
Guilherme Lima <guilhermelso2005@gmail.com>
2025-04-15 20:55:25 -0300

    feito os metodos para encontrar postagens por id e titulo e metodos post, put, delete

--- src/main/java/generation/com/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/generation/com/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,15 @@
 package generation.com.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import generation.com.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+	
 }
