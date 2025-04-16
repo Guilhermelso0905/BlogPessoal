7df9a4cfb18bf495fd8e66baf422f8ed8409b204
Guilherme Lima <guilhermelso2005@gmail.com>
2025-04-16 20:23:57 -0300

    criado os metodos de tema

---
+++ src/main/java/generation/com/blogpessoal/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package generation.com.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import generation.com.blogpessoal.model.Tema;
+
+public interface TemaRepository extends JpaRepository<Tema, Long>{
+
+	  public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+	
+	
+}
