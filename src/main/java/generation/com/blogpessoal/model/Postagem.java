8e2ca2171b9228a6213f2dd4251c1c073aff2653
Guilherme Lima <guilhermelso2005@gmail.com>
2025-05-29 22:43:29 -0300

    alterado o metodo de pegar as datas para ZonedDateTime

--- src/main/java/generation/com/blogpessoal/model/Postagem.java
+++ src/main/java/generation/com/blogpessoal/model/Postagem.java
@@ -1,6 +1,6 @@
 package generation.com.blogpessoal.model;
 
-import java.time.LocalDate;
+import java.time.ZonedDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -32,7 +32,7 @@ public class Postagem {
 	private String texto;
 
 	@UpdateTimestamp
-	private LocalDate data;
+	private ZonedDateTime data;
 
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
@@ -66,11 +66,11 @@ public class Postagem {
 		this.texto = texto;
 	}
 
-	public LocalDate getData() {
+	public ZonedDateTime getData() {
 		return data;
 	}
 
-	public void setData(LocalDate data) {
+	public void setData(ZonedDateTime data) {
 		this.data = data;
 	}
 
